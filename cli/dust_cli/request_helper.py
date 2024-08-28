51838320347db91052f0ebfaad89b9a04db4c2e1
aubin-tchoi <aubin.tchoi@gmail.com>
2024-08-28 12:05:28 +0200

    feat: add support for any http verb + improve handle_common_error_codes

--- cli/dust_cli/request_helper.py
+++ cli/dust_cli/request_helper.py
@@ -1,26 +1,37 @@
 import logging
 import os
-from typing import Any
+from typing import Any, Optional, Callable
 
 import requests
 
 
 def make_request(
-    endpoint: str, api_key: str, workspace_id: str, body: dict[str, Any]
+    verb: Callable[..., requests.Response],
+    endpoint: str,
+    api_key: str,
+    workspace_id: str,
+    body: Optional[dict[str, Any]] = None,
 ) -> requests.Response:
     """Makes a request to the Dust API."""
     # TODO: add basic logging here
-    is_dev = bool(os.environ["DUST_CLI_DEV"])
-    base_url = f"{'http://localhost:300' if is_dev else 'https://dust.tt'}/api/v1/w"
+    is_dev = bool(os.environ.get("DUST_CLI_DEV"))
+    base_url = f"{'http://localhost:3000' if is_dev else 'https://dust.tt'}/api/v1/w"
     headers = {
         "accept": "application/json",
         "content-type": "application/json",
-        "authorization": f"bearer {api_key}",
+        "authorization": f"Bearer {api_key}",
     }
 
-    response = requests.post(
-        f"{base_url}/{workspace_id}/{endpoint.strip('/')}", json=body, headers=headers
-    )
+    if body is not None:
+        response = verb(
+            f"{base_url}/{workspace_id}/{endpoint.strip('/')}",
+            json=body,
+            headers=headers,
+        )
+    else:
+        response = verb(
+            f"{base_url}/{workspace_id}/{endpoint.strip('/')}", headers=headers
+        )
     return handle_common_error_codes(response)
 
 
@@ -30,5 +41,11 @@ def handle_common_error_codes(response: requests.Response) -> requests.Response:
         return response
     if response.status_code == 401:
         logging.error("Unauthorized access, please check your API key.")
-        raise ValueError(response.content.decode())
+        raise ValueError(response.text)
+    if response.status_code == 404:
+        logging.error("Workspace not found, please check your Workspace ID.")
+        raise ValueError(response.text)
+    if response.status_code == 405:
+        logging.error("Method not supported.")
+        raise ValueError(response.text)
     return response
