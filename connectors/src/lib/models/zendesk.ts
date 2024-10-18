a5606d94e78ba042de8f41e0138639869accd609
Aubin <aubin@dust.tt>
2024-10-18 16:33:07 +0200

    feat: add data model for the help center part

--- connectors/src/lib/models/zendesk.ts
+++ connectors/src/lib/models/zendesk.ts
@@ -57,3 +57,226 @@ ZendeskConfiguration.init(
   }
 );
 ConnectorModel.hasMany(ZendeskConfiguration);
+
+export class ZendeskBrand extends Model<
+  InferAttributes<ZendeskBrand>,
+  InferCreationAttributes<ZendeskBrand>
+> {
+  declare id: CreationOptional<number>;
+  declare createdAt: CreationOptional<Date>;
+  declare updatedAt: CreationOptional<Date>;
+
+  declare name: string;
+  declare subdomain: string;
+  declare hasHelpCenter: boolean;
+
+  declare connectorId: ForeignKey<ConnectorModel["id"]>;
+}
+
+ZendeskBrand.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    subdomain: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    hasHelpCenter: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize: sequelizeConnection,
+    modelName: "zendesk_brands",
+    // TODO: see which column will be used to retrieve brands
+    indexes: [{ fields: ["connectorId"], unique: true }],
+  }
+);
+ConnectorModel.hasMany(ZendeskBrand);
+
+export class ZendeskHelpCenter extends Model<
+  InferAttributes<ZendeskHelpCenter>,
+  InferCreationAttributes<ZendeskHelpCenter>
+> {
+  declare id: CreationOptional<number>;
+  declare createdAt: CreationOptional<Date>;
+  declare updatedAt: CreationOptional<Date>;
+
+  declare name: string;
+  declare permission: "read" | "none";
+
+  declare connectorId: ForeignKey<ConnectorModel["id"]>;
+}
+
+ZendeskHelpCenter.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    permission: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize: sequelizeConnection,
+    modelName: "zendesk_help_centers",
+    indexes: [{ fields: ["connectorId"], unique: true }],
+  }
+);
+ConnectorModel.hasMany(ZendeskHelpCenter);
+
+export class ZendeskCategory extends Model<
+  InferAttributes<ZendeskCategory>,
+  InferCreationAttributes<ZendeskCategory>
+> {
+  declare id: CreationOptional<number>;
+  declare createdAt: CreationOptional<Date>;
+  declare updatedAt: CreationOptional<Date>;
+
+  declare name: string;
+  declare permission: "read" | "none";
+
+  declare connectorId: ForeignKey<ConnectorModel["id"]>;
+}
+
+ZendeskCategory.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    permission: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize: sequelizeConnection,
+    modelName: "zendesk_categories",
+    indexes: [{ fields: ["connectorId"], unique: true }],
+  }
+);
+ConnectorModel.hasMany(ZendeskCategory);
+
+export class ZendeskHelpCenterArticle extends Model<
+  InferAttributes<ZendeskHelpCenterArticle>,
+  InferCreationAttributes<ZendeskHelpCenterArticle>
+> {
+  declare id: CreationOptional<number>;
+  declare createdAt: CreationOptional<Date>;
+  declare updatedAt: CreationOptional<Date>;
+
+  declare articleId: string;
+  declare brand: string;
+  declare category: string;
+
+  declare permission: "read" | "none";
+
+  declare lastUpsertedTs: Date;
+
+  declare connectorId: ForeignKey<ConnectorModel["id"]>;
+}
+
+ZendeskHelpCenterArticle.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    articleId: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    brand: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    category: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    permission: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastUpsertedTs: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+  },
+  {
+    sequelize: sequelizeConnection,
+    modelName: "zendesk_articles",
+    indexes: [
+      {
+        fields: ["connectorId", "articleId"],
+        unique: true,
+        name: "zendesk_connector_article_idx",
+      },
+      { fields: ["articleId"] },
+      { fields: ["connectorId"] },
+    ],
+  }
+);
+ConnectorModel.hasMany(ZendeskCategory);
